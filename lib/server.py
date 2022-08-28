ce44784fb1ddabf82d24f3d132d4825ae47bbfa5
MusaTamzid05 <tamzid101@gmail.com>
2022-08-28 23:22:13 +0600

    sending click

--- lib/server.py
+++ lib/server.py
@@ -1,5 +1,7 @@
 import socket
 from pynput import mouse
+from threading import Thread
+import time
 
 
 
@@ -44,17 +46,42 @@ class Server:
 
 
 server = Server(port = 5000)
+click_count = 0
 
 def on_move(x, y):
     global server
 
-    if len(server.queue) > 0:
-        return
+
+    for item in server.queue:
+        if "mouse_pos" in item:
+            continue
 
     server.queue.append(f"mouse_pos={x},{y}")
 
+def helper_click():
+    global click_count
+    time.sleep(0.2)
+    if click_count == 1:
+        server.queue.append("single_left")
+    elif click_count == 2:
+        server.queue.append("double_left")
+
+
+    click_count = 0
+
+
+def on_click(x, y, button, pressed):
+    global click_count
+    if pressed:
+        click_count += 1
+
+    if click_count ==  1:
+        thread = Thread(target = helper_click)
+        thread.start()
+
+
 if __name__ == "__main__":
-    listener = mouse.Listener(on_move = on_move)
+    listener = mouse.Listener(on_move = on_move, on_click = on_click)
     listener.start()
     server.run()
 
