27fa315cb920a9c2a96f729c2a32a3f04813e4ad
MusaTamzid05 <tamzid101@gmail.com>
2022-08-28 21:27:24 +0600

    server

---
+++ lib/server.py
@@ -0,0 +1,35 @@
+import socket
+
+class Server:
+    def __init__(self, port):
+        self.port = port
+        self.server_socket = socket.socket()
+        self.host = socket.gethostname()
+
+        self._init_server()
+
+    def _init_server(self):
+        self.server_socket.bind((self.host, self.port))
+
+
+    def run(self):
+        server_address = socket.gethostbyname(self.host)
+        print(f"[*] Server Listening {server_address}:{self.port}")
+        self.server_socket.listen(1)
+        conn, address = self.server_socket.accept()
+
+        print("connect to :" + address)
+        conn.send("Connected".encode())
+
+
+
+
+
+
+if __name__ == "__main__":
+    server = Server(port = 5000)
+    server.run()
+
+
+
+
