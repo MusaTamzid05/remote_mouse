9873de4666faddaebc7d538005ab7e560c73deb9
musa <tamzid101@gmail.com>
2022-08-28 22:55:35 +0600

    client is receiving mouse pos

--- client.py
+++ client.py
@@ -3,14 +3,38 @@ import socket
 class Client:
     def __init__(self, host, port):
         self.client_socket = socket.socket()
-        self.client_socket.connect((host, port))
+        try:
+            self.client_socket.connect((host, port))
+            self.running = True
+        except ConnectionRefusedError as e:
+            print(e)
+            self.running = False
 
-    def test(self):
-        while True:
-            message = self.client_socket.recv(4096).decode()
-            print(message)
+    def listen(self):
+        while self.running:
+            message = self.client_socket.recv(1024).decode()
+            result = self._process_input(message)
+            if result is None:
+                continue
+            print(result)
+
+    def _process_input(self, message):
+        result = {}
+        if "mouse_pos" in message:
+            try:
+
+                mouse_data = message.split("=")
+                mouse_data = mouse_data[1].split(",")
+                result["x"] = int(mouse_data[0])
+                result["y"] = int(mouse_data[1])
+
+                return result
+            except ValueError as e:
+                print(e)
+                return None
+
 
 
 if __name__ == "__main__":
     client = Client(host = "192.168.1.3", port = 5000)
-    client.test()
+    client.listen()
