53f51fc0c527a28ab2de35ed93d5a41ac58ac905
MusaTamzid05 <tamzid101@gmail.com>
2022-08-28 21:53:57 +0600

    server is sending data

--- lib/server.py
+++ lib/server.py
@@ -12,14 +12,25 @@ class Server:
         self.server_socket.bind((self.host, self.port))
 
 
-    def run(self):
-        server_address = socket.gethostbyname(self.host)
-        print(f"[*] Server Listening {server_address}:{self.port}")
-        self.server_socket.listen(1)
-        conn, address = self.server_socket.accept()
 
-        print(f"connect to : {address[0]}")
-        conn.send("Connected".encode())
+
+
+    def run(self):
+
+        try:
+            server_address = socket.gethostbyname(self.host)
+            print(f"[*] Server Listening {server_address}:{self.port}")
+            self.server_socket.listen(1)
+            conn, address = self.server_socket.accept()
+
+            print(f"connect to : {address[0]}")
+
+            while True:
+                message = input(">> ")
+                conn.send(message.encode())
+        except KeyboardInterrupt:
+            print("[*] Closing server")
+            self.server_socket.close()
 
 
 
